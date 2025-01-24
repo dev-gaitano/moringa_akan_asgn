ac8b7d5e1fa43ed138a891ec8c08d095ed130210
dev-gaitano <dev.gaitano@gmail.com>
2025-01-25 01:39:04 +0300

    Defined a set of tasks to be perfomed by the script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// onSubmit
+
+// Create Akan name dicts / object for each gender - Day of the week index(key) : Akan name (value)
+// Get client DOB - document.get....value
+// Get Client Gender - document.get....value
+
+// Validate if client DOB is valid - Use JavaScript Operators such as '===' and '<='
+
+// Calculate DOB's Day Of The Week - Formula in the canva assignment documentation, get as Index
+// Check if the Client is Male or Female and select dict / object  based on gender - if male , else female
+// Select the Day of the week equal to our Clients Day of the week in dict - Use index calculated, validate if index is positive using modular operator
+// Return the corresponding key to the day of the week in the selected dict - Define a function to return the data to the client, the Akan name
