6f61beca9747e798287cfb113aa4c604446e3a27
dev-gaitano <dev.gaitano@gmail.com>
2025-01-25 18:16:36 +0300

    Modified notes

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const dobValidator = () => {
 }
 
 
-// Validate if client DOB is valid
+// Validate if client Gender is valid
 const genderValidator = () => {
     const dob = getClientGender();
 
@@ -44,6 +44,7 @@ const genderValidator = () => {
         return genderIsValid = false + alert('Gender is Not Valid');  
     }
 }
+
 
 // Checks if both DOB and Gender are valid
 const clientInfoValidator = () => {
@@ -59,11 +60,15 @@ const clientInfoValidator = () => {
 }
 
 
-// Listen for submit button - onSubmit , Event Listeners
+// Listen for submit button
 const submitForm = document.getElementById('submit_button').addEventListener('click', clientInfoValidator)
 
 
 // Calculate DOB's Day Of The Week - Formula in the canva assignment documentation, get as Index
+    // Takes DOB data from 'getClientDob'
+    // Calculates day of the week using canva formula & return as index
+
+
 // Check if the Client is Male or Female and select dict / object  based on gender - if male , else female
 // Select the Day of the week equal to our Clients Day of the week in dict - Use index calculated, validate if index is positive using modular operator
 // Return the corresponding key to the day of the week in the selected dict - Define a function to return the data to the client, the Akan name
