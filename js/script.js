735c71656dfdd3c53c801576b7a160de43050dd8
dev-gaitano <dev.gaitano@gmail.com>
2025-01-25 16:04:47 +0300

    Added function to return client input info
    Added function 'clientInputInfo' which:
    - Runs the 'getClientDob' and 'getClientGender' functions
    - Outputs the data from the runned functions

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 // Create Akan name dicts / object for each gender - Day of the week index(key) : Akan name (value)
 
 
-// Get client DOB - document.get....value
+// Get client DOB from input form
 const getClientDob = () => {
-    const day = document.getElementById('day').value
-    const month = document.getElementById('month').value
-    const year = document.getElementById('year').value
+    const day = parseInt(document.getElementById('day').value)
+    const month = parseInt(document.getElementById('month').value)
+    const year = parseInt(document.getElementById('year').value)
 
-    return day + month + year
+    return [day , month , year]
 };
 
 
-// Get Client Gender - document.get....value
+// Get Client Gender from input form
 const getClientGender = () => {
-    const gender = document.getElementById('gender').value 
+    const clientGender = document.getElementById('gender').value
 
-    return gender;
+    return clientGender
 };
 
+
+// Return Client input info
 const clientInputInfo = () => {
-    console.log(getClientDob());
-    console.log(getClientGender());
+    const dob = getClientDob();
+    const gender = getClientGender();
+
+    console.log(dob);
+    console.log(gender);
 }
+
 
 // Listen for submit button - onSubmit , Event Listeners
 const submitForm = document.getElementById('submit_button')
@@ -30,6 +36,9 @@ submitForm.addEventListener('click', clientInputInfo)
 
 
 // Validate if client DOB is valid - Use JavaScript Operators such as '===' and '<='
+    // If dob.day < 1 or > 31 , input invalid
+    // If dob.month < 1 or > 12 , input invalid
+    // If dob.year < 1800 or > 2100 , input invalid
 
 // Calculate DOB's Day Of The Week - Formula in the canva assignment documentation, get as Index
 // Check if the Client is Male or Female and select dict / object  based on gender - if male , else female
